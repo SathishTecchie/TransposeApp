8b8506a7d77836b04f0ddfbfc3988c4b1e598e42
Sathish_Eskilla <techolution@Sathish.local>
2018-04-02 16:57:11 +0530

    Finalcode Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [FormsModule]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  
+  // it is used to reverse the input data for single row
+  it('validataing reverseTheText method', () => {
+    component.inputString = "DBS";
+    component.reverseTheText();
+    expect(component.inputString).toBeDefined();
+    expect(component.outPutString).toEqual('SBD');
+  });
+
+  // it is used to reverse the input data for multiple rows
+  it('validataing reverseTheText method for multiple lines', () => {
+    component.inputString = "DBS\nDapeng";
+    component.reverseTheText();
+    expect(component.inputString).toBeDefined();
+    expect(component.outPutString).toEqual('gnepaD\n\nSBD');
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public inputString: string;
+  public outPutString: string;
+
+  /**
+   * @name reverseTheText
+   * @param none
+   * @returns none
+   * @desc it is used to reverse the string value
+   * @memberOf AppComponent
+   */
+  public reverseTheText(): void {
+    let outPutData = "";
+    if (this.inputString) {
+      for (let count = this.inputString.length - 1; count >= 0; count--) {
+        if (this.inputString[count] === "\n") {
+          outPutData = outPutData + "\n";
+        }
+        outPutData = outPutData + this.inputString[count];
+      }
+      this.outPutString = outPutData;
+    }
+  }
 }
